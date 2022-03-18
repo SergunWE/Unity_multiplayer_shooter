8a39d6fd7c752f8f73de96756e4e8750198da5c7
Sergun WE <sergunwe152@gmail.com>
2022-03-18 18:01:01 +0300

    Ограничение угла поворота оружия от первого лица

--- Assets/Scripts/Weapon/WeaponManager.cs
+++ Assets/Scripts/Weapon/WeaponManager.cs
@@ -159,8 +159,8 @@ public class WeaponManager : MonoBehaviour
 
     public void OnAmmunitionUpdate()
     {
-        GameCanvas.Instance.UpdateAmmunition(_weapons[_currentWeaponIndex].CurrentClip,
-            _weapons[_currentWeaponIndex].CurrentTotal);
+        GameCanvas.Instance.UpdateAmmunition(_currentWeapon.CurrentClip,
+            _currentWeapon.CurrentTotal);
     }
 
     #endregion
--- Assets/Scripts/Weapon/WeaponSway.cs
+++ Assets/Scripts/Weapon/WeaponSway.cs
@@ -6,6 +6,7 @@ public class WeaponSway : MonoBehaviour
     [SerializeField] private float smooth;
     [SerializeField] private float xSwayMultiplier;
     [SerializeField] private float ySwayMultiplier;
+    [SerializeField] private float angleLimit;
 
     private Quaternion _xRotation;
     private Quaternion _yRotation;
@@ -14,13 +15,19 @@ public class WeaponSway : MonoBehaviour
     
     private Vector2 _inputAxis;
 
+    private float _yAngle;
+    private float _xAngle;
+
     private void Update()
     {
-        _xRotation = Quaternion.AngleAxis(_inputAxis.y * xSwayMultiplier, Vector3.right);
-        _yRotation = Quaternion.AngleAxis(-_inputAxis.x * ySwayMultiplier, Vector3.up);
+        _yAngle = Mathf.Clamp(_inputAxis.y * xSwayMultiplier, -angleLimit, angleLimit);
+        _xAngle = Mathf.Clamp(_inputAxis.x * ySwayMultiplier, -angleLimit, angleLimit);
+
+        _xRotation = Quaternion.AngleAxis(_yAngle, Vector3.right);
+        _yRotation = Quaternion.AngleAxis(-_xAngle, Vector3.up);
 
         _targetRotation = _xRotation * _yRotation;
-        
+
         transform.localRotation = Quaternion.Lerp
             (transform.localRotation, _targetRotation, smooth * Time.deltaTime);
     }
