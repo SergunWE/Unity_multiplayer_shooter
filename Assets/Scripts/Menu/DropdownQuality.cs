d7dfd1a39298e543f1a3afbff8a8a82d16d5cfa6
Sergun WE <sergunwe152@gmail.com>
2022-05-31 00:20:39 +0300

    Настройка качества графики

---
+++ Assets/Scripts/Menu/DropdownQuality.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TMP_Dropdown))]
+public class DropdownQuality : MonoBehaviour
+{
+    private TMP_Dropdown _dropdown;
+    private void Awake()
+    {
+        _dropdown = GetComponent<TMP_Dropdown>();
+    }
+
+    public void SetValue(int index)
+    {
+        GraphicSettings.SetQuality(index);
+    }
+
+    private void RefreshValues()
+    {
+        _dropdown.value = GraphicSettings.GetCurrentQualityIndex();
+    }
+
+    private void OnEnable()
+    {
+        RefreshValues();
+    }
+}
--- Assets/Scripts/Menu/GraphicSettings.cs
+++ Assets/Scripts/Menu/GraphicSettings.cs
@@ -8,6 +8,7 @@ public static class GraphicSettings
     private const string DisplayWidthKey = "DisplayWidth";
     private const string DisplayHeightKey = "DisplayHeight";
     private const string DisplayRefreshRateKey = "DisplayRefreshRate";
+    private const string QualityKey = "Quality";
 
     private static readonly List<Resolution> Resolutions;
     
@@ -15,6 +16,7 @@ public static class GraphicSettings
     private static int _width;
     private static int _height;
     private static int _refreshRate;
+    private static int _quality;
 
     public static List<string> ResolutionNames { get; private set; }
 
@@ -32,9 +34,11 @@ public static class GraphicSettings
         _width = PlayerPrefs.GetInt(DisplayWidthKey, bestRes.width);
         _height = PlayerPrefs.GetInt(DisplayHeightKey, bestRes.height);
         _refreshRate = PlayerPrefs.GetInt(DisplayRefreshRateKey, bestRes.refreshRate);
+        _quality = PlayerPrefs.GetInt(QualityKey, QualitySettings.names.Length - 1);
         
         SetFullScreenMode(Convert.ToBoolean(PlayerPrefs.GetInt(FullScreenKey, 1)));
         SetResolution(_width, _height, _refreshRate);
+        SetQuality(_quality);
     }
 
     public static void SetFullScreenMode(int isFull)
@@ -53,12 +57,19 @@ public static class GraphicSettings
         SetResolution(_width, _height, _refreshRate);
     }
     
+    public static void SetQuality(int index)
+    {
+        _quality = index;
+        QualitySettings.SetQualityLevel(_quality, true);
+        PlayerPrefs.SetInt(QualityKey, _quality);
+    }
+    
     public static int GetCurrentResolutionIndex()
     {
-        if (_isFull)
-        {
-            return Resolutions.IndexOf(Screen.currentResolution);
-        }
+        // if (_isFull)
+        // {
+        //     return Resolutions.IndexOf(Screen.currentResolution);
+        // }
         var res = new Resolution
         {
             height = _height,
@@ -67,24 +78,22 @@ public static class GraphicSettings
         };
         return Resolutions.IndexOf(res);
     }
+
+    public static int GetCurrentQualityIndex()
+    {
+        return _quality;
+    }
     
     private static void SetFullScreenMode(bool isFull)
     {
         _isFull = isFull;
         Screen.SetResolution(_width, _height, _isFull, _refreshRate);
-        SaveSettings();
+        PlayerPrefs.SetInt(FullScreenKey, _isFull ? 1 : 0);
     }
-
-
+    
     private static void SetResolution(int width, int height, int refreshRate)
     {
         Screen.SetResolution(width, height, Screen.fullScreenMode, refreshRate);
-        SaveSettings();
-    }
-
-    private static void SaveSettings()
-    {
-        PlayerPrefs.SetInt(FullScreenKey, _isFull ? 1 : 0);
         PlayerPrefs.SetInt(DisplayWidthKey, _width);
         PlayerPrefs.SetInt(DisplayHeightKey, _height);
         PlayerPrefs.SetInt(DisplayRefreshRateKey, _refreshRate);
